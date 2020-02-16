57758d29fbb4166f4a196114e4f0ce33ea651d07
Vinh <qvinh@live.ca>
2020-02-15 23:16:42 -0500

    Added query suggestions

--- src/spelling_correction.py
+++ src/spelling_correction.py
@@ -2,9 +2,13 @@
 # Purpose: Provide suggestions of corrected words to the user
 
 
+import itertools
 import numpy as np
 import pandas as pd
 import re
+
+
+# TODO: maybe add weighted costs for insertion and deletion?
 
 
 # Matrix for weighted edit distance. Defined as the 1 + (1 / (substitution error + 1))
@@ -57,16 +61,57 @@ class SpellingCorrector():
         return
 
 
+    def check_query(self, query: str, limit: int = 10) -> list:
+        '''
+        Given query, use weighted edit distance to return list of alternate suggestions in order of most to least rank
+        If all words in the query are in the lexicon then return None instead
+        '''
+
+        words = query.split(' ')
+
+        # Get closest words in lexicon based on edit distance for all words in the query
+        word_suggestions = [self.check_word(word) for word in words]
+
+        # Get all combinations of suggestions - https://stackoverflow.com/questions/12935194/combinations-between-two-lists
+        suggested_queries = list(itertools.product(*word_suggestions))
+        suggested_queries = [self._join_words(word_costs) for word_costs in suggested_queries]
+
+        # Sort on total cost
+        suggested_queries.sort(key = lambda pair: pair[1])
+
+        # Limit results
+        if len(suggested_queries) > limit:
+            suggested_queries = suggested_queries[:limit]
+
+        return suggested_queries
+
+
+    def _join_words(self, word_costs: list) -> tuple:
+        '''
+        Helper function for self.check_query. Given a list of tuples (word, cost) concatenate all words and sum all costs to return (query, totalCost)
+        '''
+
+        # Get query
+        words = [word for word, _ in word_costs]
+        query = ' '.join(words)
+
+        # Get cost
+        costs = [cost for _, cost in word_costs]
+        total_cost = sum(costs)
+
+        return (query, total_cost)
+
+
     def check_word(self, word: str, same_first_letter: bool = True, limit: int = 10) -> list:
         '''
         Given a word, if the word is not in the lexicon then will calculate weighted edit distance between word and all other words
         in the lexicon and return a list of most likely to least likely word replacements in form of (word, weightedEditDistance). 
         Likelihood will be based on the weighted edit distance.
-        If the word is in the lexicon then None will be returned instead
+        If the word is in the lexicon then return (word, 0) 
         '''
 
         if word in self.lexicon: 
-            return None 
+            return [(word, 0)]
 
         # Preprocess word before comparing with lexicon
         word = self.preprocess_word(word)
