6074c5f0fbe71e66bc2ecde9174e5feb7868ccc2
pchen017@uottawa.ca <pchen017@uottawa.ca>
2020-04-13 02:29:11 -0400

    added missing eos and bos tags to wildcard bigrams and changed boolean retreival and wildcard managment to take pickle objects instead of file names

--- src/wildcard_management.py
+++ src/wildcard_management.py
@@ -2,13 +2,13 @@ import dictionary
 import pickle
 
 
-def get_inv_index():
-    return pickle.load(open("../indexes/UofO_Courses_index.pkl", "rb"))
+def get_inv_index(index_file):
+    return pickle.load(open("../models/indexes/{}".format(index_file), "rb"))
 
 
-def get_bigram_index():
+def get_bigram_index(index):
     # returns the bigram : words index
-    words = get_inv_index().dictionary.words_raw
+    words = index.dictionary.words_raw
     bigram_index = {}
     for word in words:
         if "$" + word[0] not in bigram_index:
@@ -27,24 +27,31 @@ def get_bigram_index():
 def get_wildcarded_bigrams(term):
     term_split = term.split("*")
     term_bigrams = set()
-    for section in term_split:
-        if section:
-            term_bigrams = term_bigrams.union(get_bigrams(section, False))
+    for i in range(len(term_split)):
+        endl = False
+        endr = False
+        if term_split[i]:
+            if i == 0 or (i-1 == 0 and not term_split[0]):
+                endl = True
+            if i == len(term_split) - 1 or (i - 1 == len(term_split) - 1 and not term_split[-1]):
+                endr = True
+            term_bigrams = term_bigrams.union(get_bigrams(term_split[i], endl, endr))
     return term_bigrams
 
 
-def get_bigrams(term, ends=True):
+def get_bigrams(term, endl=True, endr=True):
     term_bigrams = set()
     for i in range(len(term) - 1):
         term_bigrams.add(term[i : i + 2])
-    if ends:
+    if endl:
         term_bigrams.add("$" + term[0])
+    if endr:
         term_bigrams.add(term[-1] + "$")
     return term_bigrams
 
 
-def get_indexed_words(term):
-    bigram_index = get_bigram_index()
+def get_indexed_words(term, index_file):
+    bigram_index = get_bigram_index(index_file)
     term_bigrams = get_wildcarded_bigrams(term)
     matched_words = bigram_index[next(iter(term_bigrams))]
     for i in term_bigrams:
