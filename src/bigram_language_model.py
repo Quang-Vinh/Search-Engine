3b3ae55fb53482d8815bd2bc91d556d2f925255a
Vinh <qvinh@live.ca>
2020-04-12 21:17:12 -0400

    Added query completion

--- src/bigram_language_model.py
+++ src/bigram_language_model.py
@@ -11,7 +11,7 @@ from typing import Dict, List
 
 
 class BigramLanguageModel:
-    """Bigram language model built on given corpus and calculates P(w2|w1)
+    """Bigram language model built on given corpus. No preprocessing done other than lower case and remove special characters
     
     Attributes:
         None
@@ -20,32 +20,63 @@ class BigramLanguageModel:
     def __init__(self, corpus: List[str]):
         # Bigram model represented as {'w1': {'list of w2': {'freq', 'proba}}}
         self._bigram_model = defaultdict(self._create_dd_w2)
+        # Priors represented as {'term': {'freq': 'proba'}}
+        self._priors = {}
 
         # Get tokens and bigrams for each document within corpus
         corpus_tokens = [tokenize(doc) for doc in corpus]
         corpus_bigrams = [bigrams(doc_tokens) for doc_tokens in corpus_tokens]
 
+        # Calculate bigram freq and probabilities
         for doc_bigrams in corpus_bigrams:
             self._calculate_bigram_freq(doc_bigrams)
-
         self._calculate_bigram_proba()
+
+        # Calculate priors
+        for doc_tokens in corpus_tokens:
+            self._calculate_term_freq(doc_tokens)
+        self._calculate_term_priors()
+
+        return
+
+    def _calculate_term_freq(self, doc_tokens: List[str]) -> None:
+        """Calculates frequencies of each term
+        
+        Arguments:
+            doc_tokens {List[str]} -- List of tokens from document
+        """
+        for token in doc_tokens:
+            if token not in self._priors.keys():
+                self._priors[token] = {"freq": 1, "proba": 0}
+            else:
+                self._priors[token]["freq"] += 1
+        return
+
+    def _calculate_term_priors(self) -> None:
+        """Calculates prior probability for each individual word
+        """
+        total = 0
+        for term, val in self._priors.items():
+            total += val["freq"]
+        for term in self._priors.keys():
+            self._priors[term]["proba"] = self._priors[term]["freq"] / total
         return
 
     def _create_dd_w2(self) -> Dict:
-        '''Helper function to create default dict that can be pickled
+        """Helper function to create default dict that can be pickled
         
         Returns:
             Dict -- Default dictionary with default values
-        '''
+        """
         return defaultdict(self._create_dd_val)
 
     def _create_dd_val(self) -> Dict:
-        '''Helper function to create default dict that can be pickled
+        """Helper function to create default dict that can be pickled
         
         Returns:
             Dict -- Default values
-        '''
-        return {'freq': 0, 'proba': 0}
+        """
+        return {"freq": 0, "proba": 0}
 
     def _calculate_bigram_freq(self, doc_bigrams) -> None:
         """Calculates frequency for each bigram and stores in self._bigram_model
@@ -69,7 +100,7 @@ class BigramLanguageModel:
                 val["proba"] = val["freq"] / total_freq
         return
 
-    def get_freq(self, w1: str, w2: str) -> int:
+    def get_bigram_freq(self, w1: str, w2: str) -> int:
         """ Returns frequency of bigram from corpus
         
         Arguments:
@@ -81,8 +112,8 @@ class BigramLanguageModel:
         """
         return self._bigram_model[w1][w2]["freq"]
 
-    def get_proba(self, w1: str, w2: str) -> float:
-        """Returns probability of P(w2|w1) from corpus
+    def get_posterior(self, w1: str, w2: str) -> float:
+        """Returns posterior probability of P(w2|w1) from corpus
         
         Arguments:
             w1 {str} -- First word in bigram
@@ -93,6 +124,17 @@ class BigramLanguageModel:
         """
         return self._bigram_model[w1][w2]["proba"]
 
+    def get_prior(self, word: str) -> float:
+        """Gets prior probability P(word)
+        
+        Arguments:
+            word {str} -- Term
+        
+        Returns:
+            float -- Term prior probability
+        """
+        return self._priors[word]["proba"]
+
     def get_w2(self, w1: str) -> str:
         """Returns all words w2 that match (w1,w2) in bigram model
         
@@ -102,4 +144,28 @@ class BigramLanguageModel:
         Returns:
             str -- Matching second words in bigram
         """
-        return list(self._bigram_model[w1])
+        return list(self._bigram_model[w1].keys())
+
+    def calculate_seq_proba(
+        self, sequence: List[str], smoothing: float = 1e-6
+    ) -> float:
+        """Calculates probability of sequence by computing product of posteriors
+        
+        Arguments:
+            sequence {List[str]} -- List of words in given order
+        
+        Keyword Arguments:
+            smoothing {float} -- Smoothing parameter in case of 0 probabilities (default: {1e-6})
+        
+        Returns:
+            float -- Probability of sequence P(w_n|w_n-1) * .... * P(w_2|w_1) * P(w1)
+        """
+
+        current_word = sequence[0]
+        proba = self.get_prior(current_word)
+
+        for next_word in sequence[1:]:
+            proba *= self.get_posterior(current_word, next_word) + smoothing
+            current_word = next_word
+
+        return proba
---
+++ src/query_completion.py
@@ -0,0 +1,78 @@
+# Module 2 - Query Completion Module
+# Purpose: Provide suggestions as a list of possible completions to a query
+
+
+from bigram_language_model import BigramLanguageModel
+from sentence_preprocessing import tokenize
+from typing import List, Tuple
+
+
+class QueryCompleter:
+    """Query completer that uses a bigram language model to find suggestions for completing given queries
+    """
+
+    def __init__(self, bigram_language_model: BigramLanguageModel):
+        self.bigram_language_model = bigram_language_model
+        return
+
+    def complete_query(self, query: str, limit: int = 5) -> List[str]:
+        """Finds suggestions for completing the query 
+        
+        Arguments:
+            query {str} -- Unfinished query to complete
+
+        Keyword Arguments:
+            limit {int} -- Top amount of queries to return (default: {5})
+        
+        Returns:
+            List[str] -- List of suggested queries in form of (sentence, probability)
+        """
+        query_tokens = tokenize(query)
+
+        suggestions = self.calculate_one_step_prob(query_tokens, limit=limit)
+        suggestions = [
+            (" ".join(sent_tokens), proba) for (sent_tokens, proba) in suggestions
+        ]
+
+        return suggestions
+
+    def calculate_one_step_prob(
+        self, sent_tokens: List[str], limit: int = 5
+    ) -> List[Tuple[List[str], float]]:
+        """Calculates the one word predictions for next word in the sentence
+        
+        Arguments:
+            sent_tokens {List[str]} -- List of tokens to find most next probable word
+        
+        Keyword Arguments:
+            limit {int} -- Top amount of queries to return (default: {5})
+        
+        Returns:
+            List[Tuple[List[str], float]] -- List of most likely one step query completions in form of (sentence, probability)
+        """
+        last_word = sent_tokens[-1]
+
+        # Get possible next words from last word in sentence
+        next_words = self.bigram_language_model.get_w2(last_word)
+
+        if len(next_words) == 0:
+            return []
+
+        # Get probabilities of adding each next word P(next word | words before) = P(next_word | word at n-1) * proba
+        results = [
+            (
+                sent_tokens + [next_word],
+                self.bigram_language_model.calculate_seq_proba(
+                    sent_tokens + [next_word]
+                ),
+            )
+            for next_word in next_words
+        ]
+
+        # Sort by highest to lowest prob
+        results.sort(key=lambda tup: tup[1], reverse=True)
+
+        # Limit results
+        results = results[:limit]
+
+        return results
