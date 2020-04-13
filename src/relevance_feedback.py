39db8df32cc5ef2faef12f5561273770004d874c
Vinh <qvinh@live.ca>
2020-04-13 12:18:41 -0400

    Added rocchio relevance update algorithm

---
+++ src/relevance_feedback.py
@@ -0,0 +1,65 @@
+# Module 5 - Local Query Expansion with Rocchio Algorithm
+# Purpose: Perform implicit query expansion using the relevance provided, within the VSM, using the Rocchio Algorithm
+
+
+# TODO: remove to_full_vector and move it to vector space model
+
+
+from inverted_index import InvertedIndex
+import numpy as np
+from typing import List, Tuple
+
+
+class Rocchio():
+
+    def __init__(self, index: InvertedIndex, alpha: float = 0.8, beta: float = 0.3, gamma: float = 0.1):
+        self.dictionary = index.dictionary
+        self.index = index 
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        return
+
+    def update_query_vector(self, query_vector: List[Tuple[str, float]], relevant_doc_ids: list, non_relevant_doc_ids: list) -> List[Tuple[str, float]]:
+
+        nr_len = len(non_relevant_doc_ids)
+        r_len = len(relevant_doc_ids)
+
+        # If no relevance information then do nothing
+        if r_len == 0 and nr_len == 0:
+            return query_vector
+
+        # Get vector form of documents and queries
+        query_vector = self._to_full_vector(query_vector)
+        relevant_vectors = np.array([self.index.get_docID_vector(doc_id) for doc_id in relevant_doc_ids])
+        non_relevant_vectors = np.array([self.index.get_docID_vector(doc_id) for doc_id in non_relevant_doc_ids])
+
+        # Rocchio algorithm for update
+        relevant_vector = relevant_vectors.sum(axis=0)
+        non_relevant_vector = non_relevant_vectors.sum(axis=0)
+        updated_query_vector = query_vector
+        if r_len > 0:
+            updated_query_vector = query_vector + self.beta * (1 / r_len) * relevant_vector
+        if nr_len > 0:
+            updated_query_vector = updated_query_vector - self.gamma * (1 / nr_len) * non_relevant_vector
+
+        return updated_query_vector
+
+    def _to_full_vector(self, query_vector: List[Tuple[str, float]]) -> np.array:
+        '''Converts the sparse vector of (term, weights) into a full vector where the dimensions are the sorted terms
+        
+        Arguments:
+            query_vector {List[Tuple[str, float]]} -- Query in form (term, weights)
+        
+        Returns:
+            np.array -- Full query vector
+        '''
+        terms = list(self.index.get_terms())
+        terms.sort()
+        vector = np.zeros(len(terms))
+
+        for (term, weight) in query_vector:
+            index = terms.index(term)
+            vector[index] = weight
+
+        return vector
--- src/vector_space_model.py
+++ src/vector_space_model.py
@@ -1,13 +1,14 @@
 # Module 8 - Vector Space Model
 # Purpose: Implementing the Vector Space Model for retrieval
 
-# TODO: allow user defined weights
 # TODO: use matrix operations to more efficiently calculate similariies between query and document
+# TODO: use numpy for vector representations 
 
 import numpy as np
 import pandas as pd
 from dictionary import Dictionary
 from inverted_index import InvertedIndex
+from typing import List, Tuple
 
 
 class VectorSpaceModel:
@@ -21,8 +22,23 @@ class VectorSpaceModel:
         self.index = index
         self.dictionary = index.dictionary
         self.docIDs = self.index.docIDs
-
         return
+
+    def to_vector(self, query: str) -> List[Tuple[str, float]]:
+        '''Converts query into vector with all weights 1 for use in search
+        
+        Arguments:
+            query {str} -- Query string
+        
+        Returns:
+            List[Tuple[str, float]] -- Sparse vector 
+        '''
+        # Preprocess query string
+        query_tokens = set(self.dictionary.preprocess_document(query))
+
+        # Convert to vector with weights of 1
+        query_vector = [(query_token, 1) for query_token in query_tokens]
+        return query_vector
 
     def search(
         self,
@@ -37,18 +53,32 @@ class VectorSpaceModel:
         Uses either "inner-product" or "cosine" for similarity 
         Returns a list of tuples (docID, similarity)
         """
+        query_vector = self.to_vector(query)
+        search_results = self.vector_search(query_vector, similarity=similarity, limit=limit, include_similarities=include_similarities)
+        return search_results
+
+    def vector_search(
+        self,
+        query_vector: List[Tuple[str, float]],
+        similarity: str = "inner-product",
+        limit: int = 10,
+        include_similarities: bool = False,
+    ) -> list:
+        """
+        Given query vector weight weights searches through documents to find best matches and returns docIDs with best match, but will exclude documents with similarity of 0.
+        Query weights for each term are set to 1
+        Uses either "inner-product" or "cosine" for similarity 
+        Returns a list of tuples (docID, similarity)
+        """
         if similarity not in ["inner-product", "cosine"]:
             print("Similarity not defined")
             return None
 
-        # Preprocess query string
-        query_tokens = set(self.dictionary.preprocess_document(query))
-
         # Calculate similarity for all documents
         if similarity == "inner-product":
-            query_results = self._inner_product(query_tokens)
+            query_results = self._inner_product(query_vector)
         elif similarity == "cosine":
-            query_results = self._cosine_sim(query_tokens)
+            query_results = self._cosine_sim(query_vector)
 
         # Sort docIDs in order of decreasing similarity values
         query_results = list(query_results.items())
@@ -67,9 +97,9 @@ class VectorSpaceModel:
 
         return query_results
 
-    def _inner_product(self, query_tokens: list) -> dict:
+    def _inner_product(self, query_vector: List[Tuple[str, float]]) -> dict:
         """
-        Calculates inner product between query and all documents. Assigns each query token a weight of 1
+        Calculates inner product between query vector and all documents.
         Returns a dictionary {docID: inner_product}
         """
         # Initialize similarity for each docID
@@ -79,29 +109,27 @@ class VectorSpaceModel:
             similarities[docID] = 0
 
         # Calculate inner product for each docID
-        for word in query_tokens:
-
+        for (word, word_weight) in query_vector:
             docIDs = self.index.get_postings(word)
             if docIDs == None:
                 continue
 
             # Add weight to similarity for every document
             for docID in docIDs:
-                weight = self.index.get_tf_idf(word, docID)
-                similarities[docID] += 1 * weight
-
+                doc_weight = self.index.get_tf_idf(word, docID)
+                similarities[docID] += word_weight * doc_weight
         return similarities
 
-    def _cosine_sim(self, query_tokens: list) -> float:
+    def _cosine_sim(self, query_vector: List[Tuple[str, float]]) -> float:
         """
-        Calculates cosine similarity between query and document. Assigns each query token a weight of 1
+        Calculates cosine similarity between query and document
         """
 
-        cosine_sim = self._inner_product(query_tokens)
+        cosine_sim = self._inner_product(query_vector)
 
         # Normalize each inner product by product of lengths of query and doc vectors
         for docID, inner_prod in cosine_sim.items():
-            denom = np.sqrt(len(query_tokens)) * self._docID_length(docID)
+            denom = np.sqrt(len(query_vector)) * self._docID_length(docID)
             cosine_sim[docID] = inner_prod / denom
 
         return cosine_sim
